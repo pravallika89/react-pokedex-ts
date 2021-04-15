4d4d0123e831dcc0142bc60c9f9739ec2d4c6f7e
pravallika <pravallikaammineni@github.com>
2021-04-15 16:53:53 -0700

    created Navbar component,Homepage,pokemonpage and approuter basic structure

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,14 @@
+const HomePage=()=>{
+  return(
+    <div id="home-page">
+    <div className='row text-center mt-3'>
+        <div className='col'>
+          <h2>Welcome to the Pokedex!</h2>
+          <h4 className='text-secondary'>The Ultimate pokemon database</h4>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HomePage;
---
+++ src/pages/PokemonPage.tsx
@@ -0,0 +1,14 @@
+const PokemonPage=()=>{
+  return(
+    <div id="pokemon-page">
+      <div className="row text-center">
+        <div className="col">
+        <h4>This is Pokemon Details page</h4>
+        </div>
+      </div>
+     
+    </div>
+  )
+}
+
+export default PokemonPage;
